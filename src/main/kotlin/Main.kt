b95cc07d35725efe532d58339bcc5f8253263115
PabloLec <pablo.lecolinet@gmail.com>
2022-09-09 15:05:41 +0200

    Parse main args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
 import api.Fetcher
 
-suspend fun main(args: Array<String>) {
-    println("Program arguments: ${args.joinToString()}")
+enum class SupportedLanguages {
+    JAVASCRIPT,
+    KOTLIN,
+    JAVA
+}
 
-    val testFetcher = Fetcher("javascript")
+suspend fun main(args: Array<String>) {
+    when {
+        args.isEmpty() -> throw IllegalArgumentException("No language specified")
+        args.size > 1 -> throw IllegalArgumentException("Too many arguments")
+        args[0] !in SupportedLanguages.values()
+            .map { it.name.lowercase() } -> throw IllegalArgumentException("Unsupported language: ${args[0]}")
+    }
+
+    val language = args[0].uppercase()
+
+    val testFetcher = Fetcher(language)
     testFetcher.fetchMostStarredRepos()
 }
