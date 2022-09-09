b13fedd8519ff0b6cb8f3af1624295f73726efac
PabloLec <pablo.lecolinet@gmail.com>
2022-09-09 19:52:26 +0200

    Reformat project with ktlint

--- src/main/kotlin/models/Repository.kt
+++ src/main/kotlin/models/Repository.kt
@@ -3,28 +3,32 @@ package models
 import dev.pablolec.starrylines.MostStarredReposQuery
 import java.time.LocalDateTime
 
-
-data class Repository(val name: String, var description: String,
-                      val createdAt: LocalDateTime, var stargazers: Int, val url: String,
-                      var defaultBranch: String) {
+data class Repository(
+    val name: String,
+    var description: String,
+    val createdAt: LocalDateTime,
+    var stargazers: Int,
+    val url: String,
+    var defaultBranch: String
+) {
     companion object {
         private val dateTimeRegex = Regex("""(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})""")
 
         fun fromEdge(edge: MostStarredReposQuery.Edge): Repository {
             fun Any.toDate(): LocalDateTime {
-                val dateValues= dateTimeRegex.find(this as String)!!.groupValues
-                        .drop(1).dropLast(1).map { it.toInt() }.toTypedArray()
+                val dateValues = dateTimeRegex.find(this as String)!!.groupValues
+                    .drop(1).dropLast(1).map { it.toInt() }.toTypedArray()
                 return LocalDateTime.of(dateValues[0], dateValues[1], dateValues[2], dateValues[3], dateValues[4])
             }
 
             return Repository(
-                    edge.node!!.onRepository!!.name.trim(),
-                    edge.node.onRepository!!.description!!.trim(),
-                    edge.node.onRepository.createdAt.toDate(),
-                    edge.node.onRepository.stargazers.totalCount,
-                    edge.node.onRepository.url as String,
-                    edge.node.onRepository.defaultBranchRef!!.name
+                edge.node!!.onRepository!!.name.trim(),
+                edge.node.onRepository!!.description!!.trim(),
+                edge.node.onRepository.createdAt.toDate(),
+                edge.node.onRepository.stargazers.totalCount,
+                edge.node.onRepository.url as String,
+                edge.node.onRepository.defaultBranchRef!!.name
             )
         }
     }
-}
+} 
