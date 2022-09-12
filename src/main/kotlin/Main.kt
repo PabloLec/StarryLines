c97a2d387113e5608ecc0aa7952d847e393b1eea
PabloLec <pablo.lecolinet@gmail.com>
2022-09-12 20:29:47 +0200

    Fix regex

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,15 +14,19 @@ enum class Action {
 enum class SupportedLanguages {
     JAVASCRIPT {
         override fun extensions() = setOf(".js")
-        override fun commentRegex() = Regex("""/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*${'$'}/gm""")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
     },
     KOTLIN {
         override fun extensions() = setOf(".kt")
-        override fun commentRegex() = Regex("""/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*${'$'}/gm""")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
     },
     JAVA {
         override fun extensions() = setOf(".java")
-        override fun commentRegex() = Regex("""/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*${'$'}/gm""")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
+    },
+    PYTHON {
+        override fun extensions() = setOf(".py")
+        override fun commentRegex() = Regex("""[^:]#.*|([^(.]\"\"\"[^(]*)\"\"\"""", RegexOption.MULTILINE)
     };
 
     abstract fun extensions(): Set<String>
