b8843a4f24ea08110fd1717818e6b48a26e87a5a
PabloLec <pablo.lecolinet@gmail.com>
2022-09-12 20:32:03 +0200

    Add multiple supported languages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,12 +16,28 @@ enum class SupportedLanguages {
         override fun extensions() = setOf(".js")
         override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
     },
+    TYPESCRIPT {
+        override fun extensions() = setOf(".ts")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
+    },
     KOTLIN {
         override fun extensions() = setOf(".kt")
         override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
     },
     JAVA {
         override fun extensions() = setOf(".java")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
+    },
+    C {
+        override fun extensions() = setOf(".c")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
+    },
+    CPP {
+        override fun extensions() = setOf(".cpp")
+        override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
+    },
+    GO {
+        override fun extensions() = setOf(".go")
         override fun commentRegex() = Regex("""/\*(.|[\r\n])*?\*/|[^:]//.*""", RegexOption.MULTILINE)
     },
     PYTHON {
