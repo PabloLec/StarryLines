8a469b42306a6cacee100783623aba0d22d14ffe
PabloLec <pablo.lecolinet@gmail.com>
2022-09-14 19:37:47 +0200

    Rename parser functions

--- src/main/kotlin/models/SupportedLanguage.kt
+++ src/main/kotlin/models/SupportedLanguage.kt
@@ -1,37 +1,41 @@
 package models
 
-import loc.parseJavaStyle
+import loc.parseCStyle
 import loc.parsePythonStyle
 import java.io.BufferedReader
 
 enum class SupportedLanguage {
     JAVASCRIPT {
         override fun extensions() = setOf(".js")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     TYPESCRIPT {
         override fun extensions() = setOf(".ts")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     KOTLIN {
         override fun extensions() = setOf(".kt")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     JAVA {
         override fun extensions() = setOf(".java")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     C {
         override fun extensions() = setOf(".c")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     CPP {
         override fun extensions() = setOf(".cpp")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
+    },
+    CSHARP {
+        override fun extensions() = setOf(".cs")
+        override fun commentParser() = ::parseCStyle
     },
     GO {
         override fun extensions() = setOf(".go")
-        override fun commentParser() = ::parseJavaStyle
+        override fun commentParser() = ::parseCStyle
     },
     PYTHON {
         override fun extensions() = setOf(".py")
