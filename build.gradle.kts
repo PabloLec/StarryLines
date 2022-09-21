26773c52c90bdebcea537dac12cb2456ea57161c
PabloLec <pablo.lecolinet@gmail.com>
2022-09-21 11:17:51 +0200

    Add basic GraphQL test and mocks

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     testImplementation("io.mockk:mockk:1.12.8")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("com.apollographql.apollo3:apollo-testing-support:3.6.0")
 }
 
 tasks.test {
