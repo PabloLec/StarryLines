1eb8e63fed114e7e7ddd5c11bb12df003074d380
PabloLec <pablo.lecolinet@gmail.com>
2022-09-22 16:18:31 +0200

    Add KV support to CF workers

--- src/main/workers/index.ts
+++ src/main/workers/index.ts
@@ -1,15 +1,15 @@
 import * as Realm from "realm-web";
-import * as utils from "./utils";
 
 interface Bindings {
     REALM_APPID: string;
+    StarryLinesTop: KVNamespace;
 }
 
 type Document = globalThis.Realm.Services.MongoDB.Document;
 
 interface Repository extends Document {
     name: string;
-    descriprion: string;
+    description: string;
     createdAt: Date;
     stargazers: number;
     url: string;
@@ -23,45 +23,56 @@ let App: Realm.App;
 
 const worker: ExportedHandler<Bindings> = {
     async fetch(req, env) {
+        App = App || new Realm.App(env.REALM_APPID);
         const url = new URL(req.url);
+        const method = req.method;
+        const path = url.pathname.replace(/[/]$/, "");
+        const topID = url.pathname.split("/")[2];
+
+        // Try to get KV
+        let kv = await env.StarryLinesTop.get(topID, { cacheTtl: 7200 });
+        if (kv) {
+            console.log(`KV hit for: ${req.url}.`);
+            return reply(JSON.parse(kv));
+        }
+
+        // Fallback on edge cache
         const cacheKey = new Request(url.toString(), req);
         const cache = await caches.default;
-
         let cacheResponse = await cache.match(cacheKey);
-
         if (cacheResponse) {
             console.log(`Cache hit for: ${req.url}.`);
             return cacheResponse;
         }
 
-        App = App || new Realm.App(env.REALM_APPID);
-
-        const method = req.method;
-        const path = url.pathname.replace(/[/]$/, "");
-        const topID = url.pathname.split("/")[2];
-
         if (!path.startsWith("/api/")) {
-            return utils.toError(404);
+            return toError(404);
         }
 
         try {
             var client = await login(env.MONGO_API_KEY);
         } catch (err) {
-            return utils.toError("Error with authentication.", 500);
+            return toError("Error with authentication.", 500);
         }
 
         const collection = client.db("StarryLines").collection<Repository>(topID);
 
         try {
-            if (method !== "GET") {
-                return utils.toError(405);
-            }
-            let response = utils.reply(await collection.find());
+            if (method !== "GET") return toError(405);
+            let data = await collection.find();
+            // Remove _id from response
+            data.forEach(function (v) {
+                delete v._id;
+            });
+            let response = reply(data);
+
             await cache.put(cacheKey, response.clone());
+            await env.StarryLinesTop.put(topID, JSON.stringify(data), { expirationTtl: 7200 });
+
             return response;
         } catch (err) {
             const msg = (err as Error).message || "Error with query.";
-            return utils.toError(msg, 500);
+            return toError(msg, 500);
         }
     },
 };
@@ -70,6 +81,28 @@ async function login(token: string) {
     const credentials = Realm.Credentials.apiKey(token);
     const user = await App.logIn(credentials);
     return user.mongoClient("mongodb-atlas");
+}
+
+const cacheHeaders = {
+    "Cache-Control": "max-age=7200, s-maxage=7200",
+};
+
+function toJSON(data: unknown, status = 200): Response {
+    let headers = { "content-type": "application/json" };
+    if (status == 200) {
+        headers = { ...headers, ...cacheHeaders };
+    }
+    let body = JSON.stringify(data, null, 2);
+    return new Response(body, { headers, status });
+}
+
+function toError(error: string | unknown, status = 400): Response {
+    return toJSON({ error }, status);
+}
+
+function reply(output: any): Response {
+    if (output != null) return toJSON(output, 200);
+    return toError("Error with query", 500);
 }
 
 export default worker;
