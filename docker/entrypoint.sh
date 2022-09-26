5e8269ec961472067636e474ca5c5fd6d774f912
PabloLec <pablo.lecolinet@gmail.com>
2022-09-26 15:22:24 +0200

    Remove debug arg in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ echo "--args=${args}"
 
 ./gradlew build -x test
 ./downloadSchema.sh
-sh -c "./gradlew run --args=${args} -x test -d --stacktrace"
+sh -c "./gradlew run --args=${args} -x test --stacktrace"
