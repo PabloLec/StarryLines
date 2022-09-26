fa6266d3b1a52977a33b4bbd5e0a9c5383d61ccd
PabloLec <pablo.lecolinet@gmail.com>
2022-09-26 15:10:36 +0200

    Remove isEligible filter

--- src/main/kotlin/top/TopFactory.kt
+++ src/main/kotlin/top/TopFactory.kt
@@ -10,7 +10,6 @@ class TopFactory {
             .asSequence()
             .map { TopRepository.fromRepository(it) }
             .filterNot { it.loc == null || it.milliStarsPerLine == null }
-            .filter { isEligible(it) }
             .onEach { repo -> repo.score = getRealMilliStars(repo) }
             .sortedByDescending { it.score }
             .take(100)
@@ -19,13 +18,4 @@ class TopFactory {
     private fun getRealMilliStars(repo: TopRepository) =
         (repo.milliStarsPerLine?.div(100.0))?.times(repo.languagePercent)
             ?.toInt()
-
-    private fun isEligible(repo: TopRepository) = listOf(
-        repo.loc!! > 10,
-        "awesome" !in repo.url,
-        "curated" !in repo.description.lowercase(),
-        "obsolete" !in repo.description.lowercase(),
-        "cheatsheet" !in repo.description.lowercase()
-    )
-        .all { it }
 }
