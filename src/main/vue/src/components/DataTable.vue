e2b8beafe7d190464cc04da005de0db24d38793a
PabloLec <pablo.lecolinet@gmail.com>
2022-09-27 16:10:54 +0200

    Working data table

--- src/main/vue/src/components/DataTable.vue
+++ src/main/vue/src/components/DataTable.vue
@@ -1,59 +1,69 @@
 <template>
-    <EasyDataTable
-      :headers="headers"
-      :items="items"
-    />
-  </template>
-
+  <EasyDataTable
+    :headers="headers"
+    :items="items"
+    alternating
+    :loading="loading"
+  >
+    <template #item-name="{ name, url }">
+      <a :href="url" target="_blank" rel="noopener noreferrer">{{ name }}</a>
+    </template>
+    <template #expand="item">
+      <div style="padding: 15px">
+        {{ item.description }}
+      </div>
+    </template>
+    <template #loading>
+      <img
+        src="https://i.pinimg.com/originals/94/fd/2b/94fd2bf50097ade743220761f41693d5.gif"
+        style="width: 100px; height: 80px"
+      />
+    </template>
+  </EasyDataTable>
+</template>
 
 <script>
-    import Vue3EasyDataTable from 'vue3-easy-data-table';
-    import 'vue3-easy-data-table/dist/style.css';
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
-
-    export default {
-      components: {
-        'EasyDataTable': Vue3EasyDataTable
-      },
-      data () {
-          return {
-            language: "kotlin",
-            headers: [
-              { text: "PLAYER", value: "player" },
-              { text: "TEAM", value: "team"},
-              { text: "NUMBER", value: "number"},
-              { text: "POSITION", value: "position"},
-              { text: "HEIGHT", value: "indicator.height"},
-              { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true},
-              { text: "LAST ATTENDED", value: "lastAttended", width: 200},
-              { text: "COUNTRY", value: "country"},
-            ],
-            items: [
-              { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: {"height": '6-2', "weight": 185}, lastAttended: "Davidson", country: "USA"},
-              { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: {"height": '6-9', "weight": 250}, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA"},
-              { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: {"height": '6-10', "weight": 240}, lastAttended: "Texas-Austin", country: "USA"},
-              { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: {"height": '6-11', "weight": 242}, lastAttended: "Filathlitikos", country: "Greece"},
-            ],
-          }
-        },
-        methods: {
-            async fetchData() {
-                this.items = null
-                const res = await fetch(
-                    `https://starrylines.pablolec.dev/api/${this.language}_top`
-                )
-                this.items = await res.json()
-            }
-        },
-        mounted() {
-            this.fetchData()
-        },
-        watch: {
-            language() {
-                this.fetchData()
-            }
-        }
+export default {
+  components: {
+    EasyDataTable: Vue3EasyDataTable,
+  },
+  data() {
+    return {
+      language: "kotlin",
+      headers: [],
+      items: [],
+      loading: true,
     };
-
-
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      this.items = null;
+      const res = await fetch(
+        `https://starrylines.pablolec.dev/api/${this.language}_top`
+      );
+      this.items = await res.json();
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.headers = [
+      { text: "name", value: "name", sortable: true },
+      { text: "createdAt", value: "createdAt", sortable: true },
+      { text: "stargazers", value: "stargazers", sortable: true },
+      { text: "loc", value: "loc", sortable: true },
+      { text: "milliStarsPerLine", value: "milliStarsPerLine" },
+      { text: "score", value: "score", sortable: true },
+    ];
+    this.fetchData();
+  },
+  watch: {
+    language() {
+      this.fetchData();
+    },
+  },
+};
 </script>
