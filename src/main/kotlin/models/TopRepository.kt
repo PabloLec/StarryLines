a2ff935075a7cdd5d878357a901b774153bed8a3
PabloLec <pablo.lecolinet@gmail.com>
2022-09-27 16:34:58 +0200

    Store rank on top repositories

--- src/main/kotlin/models/TopRepository.kt
+++ src/main/kotlin/models/TopRepository.kt
@@ -1,30 +1,34 @@
 package models
 
-import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class TopRepository(
+    var rank: Int?,
     val name: String,
     val description: String,
-    val createdAt: LocalDate,
+    val createdAt: String,
     val stargazers: Int,
     val url: String,
-    val loc: Int?,
-    val milliStarsPerLine: Int?,
+    val loc: Int,
+    val languagePercent: Int,
+    val milliStarsPerLine: Int,
     var score: Int?
 ) {
-    var languagePercent: Int = 0
 
     companion object {
+        private val formatter: DateTimeFormatter? = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         fun fromRepository(repo: Repository): TopRepository =
             TopRepository(
+                null,
                 repo.name,
                 repo.description,
-                repo.createdAt.toLocalDate(),
+                repo.createdAt.format(formatter),
                 repo.stargazers,
                 repo.url,
-                repo.loc,
-                repo.milliStarsPerLine,
+                repo.loc!!,
+                repo.languagePercent,
+                repo.milliStarsPerLine!!,
                 null
-            ).also { it.languagePercent = repo.languagePercent }
+            )
     }
 }
