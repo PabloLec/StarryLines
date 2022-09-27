e44747952770b6d72563fc79dfc05bfbbce54e08
PabloLec <pablo.lecolinet@gmail.com>
2022-09-27 20:37:16 +0200

    Add partial responsiveness on data table

--- src/main/vue/tailwind.config.js
+++ src/main/vue/tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {},
+    fontSize: {
+      0: 0,
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
   },
   variants: {
     extend: {},
