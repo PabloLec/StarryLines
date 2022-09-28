914704965e02455cdca4c1da5ddcb78fe421d8cc
PabloLec <pablo.lecolinet@gmail.com>
2022-09-28 18:19:08 +0200

    Testing webpack config

--- src/main/vue/vue.config.js
+++ src/main/vue/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    optimization: {
+      nodeEnv: "production",
+      concatenateModules: true,
+      mergeDuplicateChunks: true,
+      minimize: true,
+    },
+  },
 });
