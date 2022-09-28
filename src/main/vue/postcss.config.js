06f9f24f635d76dafd20b2891a903cd5657904ea
PabloLec <pablo.lecolinet@gmail.com>
2022-09-28 20:24:38 +0200

    Testing purgeCSS config

--- src/main/vue/postcss.config.js
+++ src/main/vue/postcss.config.js
@@ -1,6 +1,26 @@
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    true &&
+      purgecss({
+        content: [`./public/**/*.html`, `./src/**/*.vue`, `./node_modules/vue3-easy-data-table/**/*.js`],
+        defaultExtractor(content) {
+          const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
+          return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+        },
+        safelist: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/,
+          /swiper*/,
+          /direction-center/,
+        ],
+      }),
+  ],
 };
