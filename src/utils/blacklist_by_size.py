d1fd83fe43d44560c3c21f6caccd443ece82670b
PabloLec <pablo.lecolinet@gmail.com>
2022-09-29 16:12:59 +0200

    Update utils scripts

--- src/utils/blacklist_by_size.py
+++ src/utils/blacklist_by_size.py
@@ -3,18 +3,23 @@ from common import db, LANGS, add_to_blacklist
 MAX_SIZE = 1_000_000  # 1GB
 
 
-def get_repos_too_big(coll_name: str):
+def get_repos_to_blacklist(coll_name: str):
     c = db[coll_name]
-    to_blacklist = set()
+    too_big = set()
+    too_small = set()
     for repo in c.find({}):
         if repo["diskUsage"] > MAX_SIZE:
-            to_blacklist.add(repo["url"])
-    return to_blacklist
+            too_big.add(repo["url"])
+        elif repo["loc"] < 15:
+            too_small.add(repo["url"])
+    return too_big, too_small
 
 
 def main():
     for lang in LANGS:
-        add_to_blacklist(get_repos_too_big(lang), "REPO TOO BIG")
+        too_big, too_small = get_repos_to_blacklist(lang)
+        add_to_blacklist(too_big, "REPO TOO BIG")
+        add_to_blacklist(too_small, "REPO TOO SMALL")
 
 
 main()
