6e330c7636b806572e9c9903e0830eb0ef513c02
PabloLec <pablo.lecolinet@gmail.com>
2022-09-29 19:06:14 +0200

    Switch to Vite

--- src/main/vue/src/App.vue
+++ src/main/vue/src/App.vue
@@ -1,41 +1,41 @@
 <template>
-  <LangSlider @clickLanguage="changeLanguage" />
-  <DataTable :language="language" />
-</template>
+    <LangSlider @clickLanguage="changeLanguage" />
+    <DataTable :language="language" />
+  </template>
 
-<script>
-import DataTable from "./components/DataTable.vue";
-import LangSlider from "./components/LangSlider.vue";
+  <script>
+  import DataTable from "./components/DataTable.vue";
+  import LangSlider from "./components/LangSlider.vue";
 
-export default {
-  name: "App",
-  components: {
-    LangSlider,
-    DataTable,
-  },
-  data() {
-    return {
-      language: "kotlin",
-    };
-  },
-  mounted() {
-    document.body.classList.add("mx-0", "sm:mx-12", "md:mx-16", "lg:mx-20");
-  },
-  methods: {
-    changeLanguage(lang) {
-      this.language = lang;
+  export default {
+    name: "App",
+    components: {
+      LangSlider,
+      DataTable,
     },
-  },
-};
-</script>
+    data() {
+      return {
+        language: "kotlin",
+      };
+    },
+    mounted() {
+      document.body.classList.add("mx-0", "sm:mx-12", "md:mx-16", "lg:mx-20");
+    },
+    methods: {
+      changeLanguage(lang) {
+        this.language = lang;
+      },
+    },
+  };
+  </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
--- src/main/vue/tailwind.config.cjs
+++ src/main/vue/tailwind.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontSize: {
