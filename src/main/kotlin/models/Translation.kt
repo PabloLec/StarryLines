9308df4e59c9955695e41abd1c02f3cd4adc68df
PabloLec <pablo.lecolinet@gmail.com>
2022-11-12 13:31:14 +0100

    Fix DEEPL JSON key

--- src/main/kotlin/models/Translation.kt
+++ src/main/kotlin/models/Translation.kt
@@ -1,6 +1,9 @@
 package models
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Translation(
+    @JsonProperty("detected_source_language")
     val detectedSourceLanguage: String,
     val text: String
 )
